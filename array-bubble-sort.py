9f72757335f401263569f5b6c1d0f21c42c7ad5d
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-02-04 20:59:14 +0530

    Update array-bubble-sort.py

--- array-bubble-sort.py
+++ array-bubble-sort.py
@@ -10,8 +10,13 @@ no_arr = len(arr)
 
 #loop through array
 for i in range(no_arr-1):
+    swapped = False #original value of swapped
     for j in range(no_arr-i-1):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
+            swapped=True #changing the value if value swaps
+    
+    if not swapped:
+        break 
         
 print('Sorted array: ', arr)
