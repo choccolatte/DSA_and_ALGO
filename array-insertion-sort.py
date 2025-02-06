ebd53fb8d0d0867c7757158fd9995a12bb2adc25
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-02-06 20:59:04 +0530

    Update array-insertion-sort.py
    made sorting better

--- array-insertion-sort.py
+++ array-insertion-sort.py
@@ -8,18 +8,41 @@ print('Unsorted array: ', arr)
 # length of array
 len_array=len(arr)
 
+# #looping through array - outer
+# for i in range(1, len_array):
+#     insert_index = i
+#     current_value = arr.pop(i)
+        
+#     #looping through array - inner
+
+#     for j in range(i-1, -1, -1):
+#         if arr[j] > current_value:
+#             insert_index = j
+
+#     arr.insert(insert_index, current_value)
+ 
+# #printing sorted array
+# print('Sorted array: ', arr)
+
+
+# insertion sort - improved version
+
 #looping through array - outer
 for i in range(1, len_array):
     insert_index = i
-    current_value = arr.pop(i)
+    current_value = arr[i]
         
     #looping through array - inner
 
     for j in range(i-1, -1, -1):
         if arr[j] > current_value:
+            arr[j+1] = arr[j]
             insert_index = j
 
-    arr.insert(insert_index, current_value)
+        else:
+            break
+
+    arr[insert_index] = current_value
  
 #printing sorted array
 print('Sorted array: ', arr)
