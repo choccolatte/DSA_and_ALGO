79dba4a79103e2f9f9a5588f1908e7e1882268a4
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-02-16 18:35:25 +0530

    Update array-binary-search.py
    inserted selection sort

--- array-binary-search.py
+++ array-binary-search.py
@@ -1,15 +1,14 @@
 import random as rd
-import timeit as tt
 
-arr = list(range(1, 10001))
-# rd.shuffle(arr)
+arr = list(range(1, 101))
+rd.shuffle(arr)
 
 print('Unsorted array: ', arr)
 
 #deciding a target val - letting the user do it
-targetVal = int(input('Enter target value to find (0-10000): '))
+targetVal = int(input('Enter target value to find (0-100): '))
 
-#defining linear search algo
+#defining binary search algo
 def binarySearch(arr, targetVal):
     left = 0
     right = len(arr) - 1
@@ -28,7 +27,25 @@ def binarySearch(arr, targetVal):
 
     return -1
 
+
+# adding a sorting algo here to sort the unsorted array
+# length of array
+len_array=len(arr)
+
+#looping through array - outer
+for i in range(len_array):
+    min_index = i
+    
+    #looping through array - inner
+
+    for j in range(i+1, len_array):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+ 
+#printing sorted array
+print('Sorted array: ', arr)
+
 result = binarySearch(arr, targetVal)
 print(result)
-
-# enter a sorting algo here to sort the unsorted array
